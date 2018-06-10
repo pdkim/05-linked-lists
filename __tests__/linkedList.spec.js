3a458584ccd6428736bfd021e2c64122d2e724ae
Phillip Kim <phildkim@gmail.com>
2018-06-10 13:27:47 -0700

    getting started

---
+++ __tests__/linkedList.spec.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const list = require('../lib/linkedList.js');
+
+describe('Linked lists', () => {
+
+  //append
+  xit('should insert a new node to the end of the list', (done) => {
+    let expected = 0;
+    let actual = 0;
+    expect(actual).toEqual(expected);
+    done();
+  });
+
+  //prepend
+  xit('should add new node before specific node', (done) => {
+    let actual = 0;
+    let expected = 0;
+    expect(actual).toEqual(expected);
+    done();
+  });
+  
+  //reverse
+  xit('should return a linked list in reverse order of the previous one', (done) => {
+    let actual = 0;
+    let expected = 0;
+    expect(actual).toEqual(expected);
+    done();
+  });
+
+  //serialize
+  xit('should return a linked list in reverse order of the previous one', (done) => {
+    let actual = 0;
+    let expected = 0;
+    expect(actual).toEqual(expected);
+    done();
+  });
+
+  //deserialize
+  xit('should return a linked list in reverse order of the previous one', (done) => {
+    let actual = 0;
+    let expected = 0;
+    expect(actual).toEqual(expected);
+    done();
+  });
+  
+});
